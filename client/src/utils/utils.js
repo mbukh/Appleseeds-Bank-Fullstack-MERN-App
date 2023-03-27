bd0a91562c86e7cd59ec88d545bb245851a6ff61
mbukhman <mbukhman@gmail.com>
2023-03-27 16:51:32 +0400

    refactoring

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,4 @@
-export const parseDate = (dateString, options = { time: false }) => {
+export const formatDate = (dateString, options = { time: false }) => {
     const date = new Date(dateString);
     const { time } = options;
 
@@ -11,10 +11,10 @@ export const parseDate = (dateString, options = { time: false }) => {
     });
 };
 
-export const renderValue = (entry, key) => {
+export const formatDisplayValue = (entry, key) => {
     if (key === "id") return "..." + entry[key].slice(-6);
     if (key === "updatedAt" && entry.createdAt === entry.updatedAt) return "never";
     if (key === "createdAt" || key === "updatedAt")
-        return parseDate(entry[key], { time: key === "updatedAt" });
+        return formatDate(entry[key], { time: key === "updatedAt" });
     return entry[key];
 };
